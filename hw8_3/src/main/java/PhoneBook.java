256e61f079fcaab1f537ede7db60aa3c9b19cfba
ksa237 <ksa237@yandex.ru>
2025-11-24 20:16:29 +1000

    Stub tests for add(1commit), add implemented(2commit)

--- hw8_3/src/main/java/PhoneBook.java
+++ hw8_3/src/main/java/PhoneBook.java
@@ -1,2 +1,12 @@
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    private TreeMap<String, String> contacts = new TreeMap<>();
+
+    public int add(String name, String phoneNumber) {
+        contacts.put(name, phoneNumber);
+        return contacts.size();
+    }
+
 }
--- hw8_3/src/test/java/PhoneBookTest.java
+++ hw8_3/src/test/java/PhoneBookTest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PhoneBookTest {
+
+
+    @Test
+    void testAddMethod() {
+        PhoneBook phoneBook = new PhoneBook();
+
+        int result1 = phoneBook.add("Andrey", "89025523412");
+        assertEquals(1, result1, "Количество абонентов после добавления невеное");
+
+        int result2 = phoneBook.add("Marina", "89245556123");
+        assertEquals(2, result2, "Количество абонентов после добавления невеное");
+
+    }
+
+
 }
