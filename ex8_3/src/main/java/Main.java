4bc29bcaa49599b44835d3a4314b58389bbb888b
ksa237 <ksa237@yandex.ru>
2025-11-24 18:38:30 +1000

    Initial commit

---
+++ ex8_3/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Log log = new Log();
+
+        log.log("INFO", "Hello");
+        log.log("ERROR", "Произошла ошибка");
+        log.log("INFO", "Bye");
+
+
+
+    }
+
+}
